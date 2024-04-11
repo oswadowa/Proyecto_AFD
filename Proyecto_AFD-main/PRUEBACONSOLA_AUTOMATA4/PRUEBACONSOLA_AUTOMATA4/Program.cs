589163a75c2b2a69ea50bb0dbf17b226d3a5edce
UrlDarc <71182156+UrlDarc@users.noreply.github.com>
2024-04-11 11:58:55 -0600

    Add files via upload

---
+++ Proyecto_AFD-main/PRUEBACONSOLA_AUTOMATA4/PRUEBACONSOLA_AUTOMATA4/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace PRUEBACONSOLA_AUTOMATA4
+{
+    public class Automata
+    {
+        private enum Estado { q0, q1, q2, q3 }
+        private Estado estadoActual;
+
+        public Automata()
+        {
+            estadoActual = Estado.q0;
+        }
+
+        public bool EsValida(string cadena)
+        {
+            foreach (char simbolo in cadena)
+            {
+                estadoActual = Transicion(estadoActual, simbolo);
+                if (estadoActual == Estado.q0)
+                {
+                    return false;
+                }
+            }
+
+            return estadoActual == Estado.q3;
+        }
+
+        public string ObtenerRecorrido(string cadena)
+        {
+            string recorrido = "";
+            Estado estadoTemporal = estadoActual; // Guardamos el estado actual en una variable temporal
+
+            foreach (char simbolo in cadena)
+            {
+                recorrido += $"{estadoTemporal} , {simbolo} , ";
+                estadoTemporal = Transicion(estadoTemporal, simbolo);
+                recorrido += $"{estadoTemporal}\n";
+            }
+
+            return recorrido;
+        }
+
+        private Estado Transicion(Estado estadoActual, char simbolo)
+        {
+            switch (estadoActual)
+            {
+                case Estado.q0:
+                    return simbolo == 'a' ? Estado.q2 : Estado.q1;
+                case Estado.q1:
+                    return simbolo == 'a' ? Estado.q1 : Estado.q3;
+                case Estado.q2:
+                    return simbolo == 'a' ? Estado.q3 : Estado.q2;
+                case Estado.q3:
+                    return simbolo == 'c' ? Estado.q0 : Estado.q3;
+                default:
+                    throw new Exception("Estado no válido");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automata automata = new Automata();
+
+            Console.WriteLine("Ingrese una cadena:");
+            string cadena = Console.ReadLine();
+
+            Automata automataCopia = new Automata(); // Creamos una copia del autómata
+
+            if (automataCopia.EsValida(cadena))
+            {
+                Console.WriteLine("La cadena es válida");
+                Console.WriteLine("Recorrido:");
+                Console.WriteLine(automata.ObtenerRecorrido(cadena)); // Utilizamos la copia para obtener el recorrido
+            }
+            else
+            {
+                Console.WriteLine("La cadena no es válida");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Proyecto_AFD-main/Proyecto AFD Fase II/Proyecto AFD Fase II/Form2.cs
@@ -0,0 +1,94 @@
+﻿using Proyecto_AFD_Fase_II;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace CargaArchivo
+{
+    public partial class Form2 : Form
+    {
+        public Form1 form1;
+        public Form2(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtarchivos.SelectedIndex != -1)
+            {
+                //C:\Users\David\Downloads\Archivos
+
+                String Automata = txtarchivos.SelectedItem.ToString();
+                form1.lblautoselect.Visible = true;
+                form1.lblautoselect.Text = Automata;
+
+                String RutaAutomata = ruta + "\\" + Automata;
+
+                try
+                {
+                    // Verificar si el archivo existe
+                    if (File.Exists(RutaAutomata))
+                    {
+                        // Leer todo el texto del archivo
+                        string text = File.ReadAllText(RutaAutomata);
+
+                        // Mostrar el contenido del archivo en el TextBox
+                        form1.txtlistaarchivos.Text = text;
+                        this.Hide();
+                    }
+                    else
+                    {
+                        form1.txtlistaarchivos.Text = "El archivo no existe.";
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+                    form1.txtlistaarchivos.Text = "Error al leer el archivo: " + ex.Message;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, Por favor seleccione 1 de los archivos .txt");
+            }
+            
+        }
+
+        public string ruta;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ruta = txtdireccion.Text;
+
+            // Obtener la lista de archivos en el directorio
+            string[] files = Directory.GetFiles(ruta);
+
+            // Agregar los nombres de los archivos al ListBox
+            foreach (string file in files)
+            {
+                txtarchivos.Items.Add(Path.GetFileName(file));
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
